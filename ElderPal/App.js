5d8412dd2a0c259a5ccaacc6d1282c6f231149bf
Anuja-HPK <anujahpk@gmail.com>
2024-02-06 22:16:55 +0530

    Creating Log In screen
    The 1st screen which shows log in button and sign up button for the user.

---
+++ ElderPal/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, SafeAreaView, Platform, Button } from 'react-native';
+import LogInScreen from './app/screens/LogInScreeen';
+
+export default function App() {
+  return (
+   <LogInScreen/>
+  );
+}
+
+
---
+++ ElderPal/app/screens/LogInScreeen.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.appName}>ElderPal</Text>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../assets/Logo.png')} 
+          style={styles.logo}
+        />
+      </View>
+      
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => console.log('Login Pressed')}>
+          <Text style={styles.buttonTextin}>Log In</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => console.log('Sign Up Pressed')}>
+          <Text style={styles.buttonTextup}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#199BC3',
+  },
+  appName: {
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 30,
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 220, 
+    height: 220, 
+    resizeMode: 'contain',
+  },
+  buttonContainer: {
+    marginTop: 50,
+    flexDirection: 'column', 
+  },
+  button: {
+    backgroundColor: '#29CF9D',
+    borderRadius: 45, // curving edges
+    paddingVertical: 22, // height increase
+    paddingHorizontal: 70, // length increase
+    marginBottom: 15, // gap between buttons
+  },
+  buttonTextin: {
+    color: '#fff',
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  buttonTextup: {
+    color: 'black',
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
