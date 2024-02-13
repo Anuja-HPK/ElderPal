eb3bc360e5a467f1d5c3ef1309f1dd2b1e84f99a
Liviru Jayawardhana <livirujayawardhana2001@gmail.com>
2024-02-14 02:02:35 +0530

    Add UI elements

--- ElderPal/app/screens/SignInScreen.js
+++ ElderPal/app/screens/SignInScreen.js
@@ -1,26 +1,36 @@
-import React, { useState } from 'react';
-import {View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground, Image} from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Image,
+} from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const SignInScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
 
-  const backgroundImage = require('../assets/Elder care person.jpg');
+  const backgroundImage = require("../assets/Elder care person.jpg");
 
   return (
     <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
       <View style={styles.overlay}>
-      <Image
-        source={require('../assets/Logo.png')}
-        style={styles.logo}
-      />
+        <Image source={require("../assets/Logo.png")} style={styles.logo} />
         <Text style={styles.title}>Sign In</Text>
 
         <Text style={styles.label}>Username</Text>
         <View style={styles.inputContainer}>
-          <MaterialCommunityIcons name="account" size={30} color="#fff" style={styles.icon} />
+          <MaterialCommunityIcons
+            name="account"
+            size={30}
+            color="#fff"
+            style={styles.icon}
+          />
           <TextInput
             style={[styles.input]}
             placeholder="Enter Your Username"
@@ -46,7 +56,7 @@ const SignInScreen = () => {
             onPress={() => setPasswordVisible(!passwordVisible)}
           >
             <MaterialCommunityIcons
-              name={passwordVisible ? 'eye-off' : 'eye'}
+              name={passwordVisible ? "eye-off" : "eye"}
               size={24}
               color="#00b33c"
             />
@@ -59,7 +69,11 @@ const SignInScreen = () => {
 
         <View style={styles.signupContainer}>
           <Text style={styles.signupText}>Don't have an account?</Text>
-          <TouchableOpacity onPress={() => {/* Navigate to Sign Up */}}>
+          <TouchableOpacity
+            onPress={() => {
+              /* Navigate to Sign Up */
+            }}
+          >
             <Text style={styles.signupButton}> Sign Up Here</Text>
           </TouchableOpacity>
         </View>
@@ -74,104 +88,104 @@ const styles = StyleSheet.create({
     height: 100,
     alignSelf: "center",
     borderWidth: 2,
-    borderColor: '#00b33c',
+    borderColor: "#00b33c",
     borderRadius: 90,
   },
 
   backgroundImage: {
     flex: 1,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 
   overlay: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
   },
 
   title: {
     fontSize: 60,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    color:  '#fff',
+    color: "#fff",
   },
 
   label: {
-    alignSelf: 'flex-start',
-    marginLeft: '5%',
+    alignSelf: "flex-start",
+    marginLeft: "5%",
     fontSize: 20,
-    fontWeight: 'bold',
-    color:  '#4dff88',
+    fontWeight: "bold",
+    color: "#4dff88",
   },
 
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '90%',
+    flexDirection: "row",
+    alignItems: "center",
+    width: "90%",
     margin: 12,
     borderWidth: 2,
     borderRadius: 20,
-    borderColor: '#000000',
+    borderColor: "#000000",
   },
 
   icon: {
-    position: 'absolute',
+    position: "absolute",
     right: 10,
-    color: '#00b33c'
+    color: "#00b33c",
   },
 
   input: {
-    width: '90%',
+    width: "90%",
     height: 50,
     padding: 10,
-    color: '#fff',
+    color: "#fff",
   },
 
   passwordInput: {
     flex: 1,
     marginRight: 44,
-    color: '#fff',
+    color: "#fff",
   },
 
   eyeIcon: {
-    position: 'absolute',
+    position: "absolute",
     right: 10,
   },
 
   button: {
-    width: '60%',
-    alignItems: 'center',
-    backgroundColor: '#00b33c',
+    width: "60%",
+    alignItems: "center",
+    backgroundColor: "#00b33c",
     padding: 15,
     borderRadius: 30,
     marginTop: 20,
   },
-  
+
   buttonText: {
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
 
   signupContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  
+
   signupText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color:  '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
-  
+
   signupButton: {
     fontSize: 18,
-    color: '#4dff88',
-    fontWeight: 'bold',
+    color: "#4dff88",
+    fontWeight: "bold",
   },
 });
 
--- ElderPal/app/screens/TodoList.js
+++ ElderPal/app/screens/TodoList.js
@@ -10,22 +10,64 @@ import React from "react";
 export default function TodoList() {
   return (
     <ImageBackground
-      source={require("../assets/todo_home.jpg")}
+      source={require("../assets/todo_home.jpg")} //background image
       style={{ width: "100%", height: "100%" }}
     >
       <ScrollView>
-        <View style={{ alignItems: "center" }}>
+        <View
+          style={{
+            alignItems: "center",
+            marginBottom: 30,
+            paddingBottom: -70,
+            borderBottomRightRadius: 70,
+            borderBottomLeftRadius: 70,
+            backgroundColor: "rgba(128, 128, 128, 0.3)",
+          }}
+        >
           <Text
             style={{
-              marginVertical: 60,
+              marginVertical: 60, //vertical margin
               fontSize: 50,
               fontWeight: 800,
             }}
           >
             TodoList
           </Text>
-          <TouchableOpacity>
-            <Text>Add a New Task</Text>
+        </View>
+
+        <View style={{ marginLeft: 15 }}>
+          <TouchableOpacity
+            style={{
+              marginRight: 120,
+              paddingLeft: 30,
+              paddingTop: 10,
+              paddingBottom: 10,
+              borderRadius: 30,
+              backgroundColor: "rgba(128, 128, 128, 0.8)",
+            }}
+          >
+            <Text style={{ fontSize: 25, fontWeight: 400}}>
+              + Add a New Task
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            marginRight: 30,
+            alignItems: "flex-end",
+            marginVertical: 500,
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              paddingLeft: 22,
+              paddingRight: 22,
+              borderRadius: 50,
+              backgroundColor: "rgba(128, 128, 128, 0.8)",
+            }}
+          >
+            <Text style={{ fontSize: 50, fontWeight: 300 }}>+</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
