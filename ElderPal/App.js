af53044578c4c1d8d32c85cb32d9125ba080bbe3
DharshanSR <dharshanravindran8@gmail.com>
2024-02-21 21:52:18 +0530

    Welcome screen

--- ElderPal/App.js
+++ ElderPal/App.js
@@ -4,11 +4,12 @@ import ChooseRoleScreen from "./app/screens/ChooseRoleScreen";
 import SignInScreen from  "./app/screens/SignInScreen";
 import ElderSignUp from  "./app/screens/ElderSignUp";
 import CommonDBDoctor from "./app/screens/CommonDBDoctor";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
     <>
-      <ChooseRoleScreen />
+      <WelcomeScreen />
     </>
   );
 }
