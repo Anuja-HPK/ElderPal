2920be9938db8695caf3a48e0b2ac36e51489443
DharshanSR <dharshanravindran8@gmail.com>
2024-02-22 18:04:48 +0530

    welcome screen

---
+++ ElderPal/app/screens/WelcomeScreen.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, Animated, ImageBackground } from 'react-native';
+
+const WelcomeScreen = ({ navigation }) => {
+  const [fadeAnim] = useState(new Animated.Value(0)); // Initial value for opacity: 0
+
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1, // Fade in to full opacity
+        duration: 4000, // Duration of the animation
+        useNativeDriver: true, // Use native driver for better performance
+      }
+    ).start();
+
+    const timer = setTimeout(() => {
+      /*navigation.navigate('SignInScreen'); */// Make sure this matches the name you've given the SignInScreen in your navigation stack
+    }, 3000); // Navigate after 3 seconds
+
+    return () => clearTimeout(timer);
+  }, [fadeAnim, navigation]);
+
+  return (
+    <ImageBackground 
+      source={require('../assets/welcome screen image.jpg')} 
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Animated.View style={{...styles.logoContainer, opacity: fadeAnim}}>
+          <Image
+            source={require('../assets/Logo.png')}
+            style={styles.logo}
+          />
+        </Animated.View>
+        <Animated.View style={{opacity: fadeAnim}}>
+          <Text style={styles.title}>Welcome to ElderPal</Text>
+          <Text style={styles.description}>
+          Stay connected, supported, and informed with all your elder care needs in one place.
+          </Text>
+        </Animated.View>
+      </View>
+    </ImageBackground>
+  );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      // Remove backgroundColor from container since the background image will cover it
+    },
+
+    backgroundImage: {
+      flex: 1,
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    logo: {
+      width: 160,
+      height: 160,
+      marginBottom: 100,
+    },
+
+    logoContainer: {
+      alignItems: 'center',
+      marginBottom: 30,
+    },
+
+    title: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    
+    description: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+        lineHeight: 24,
+        marginTop: 30,
+        marginBottom: 50,
+        paddingHorizontal: 20,
+    }
+});
+  
+  
+export default WelcomeScreen;
