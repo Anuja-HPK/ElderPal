3dd91842fe0fa137e87f3b7e745a9b1bf6faaeca
janithi01 <143939824+janithi01@users.noreply.github.com>
2024-03-13 10:45:25 +0530

    Create index.js
    dummy texts to build the chat screen

---
+++ ElderPal/app/constants/index.js
@@ -0,0 +1,19 @@
+export const apiKey = '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';
+export const dummyMessages = [
+    {
+        role: 'user',
+        content: 'How are you?'
+    },
+    {
+        role: 'assistant',
+        content: "I'm fine, How may i help you today."
+    },
+    {
+        role: 'user',
+        content: 'create an image of a dog playing with cat'
+    },
+    {
+        role: 'assistant',
+        content: 'https://storage.googleapis.com/pai-images/ae74b3002bfe4b538493ca7aedb6a300.jpeg'
+    }
+]
