97fe35215ad99ccbc6b08987d67f9a273f73b460
janithi01 <143939824+janithi01@users.noreply.github.com>
2024-03-13 10:59:19 +0530

    Create assistantwelcome.js
    screen for the user to first enter to the assistant section

---
+++ ElderPal/app/screens/assistantwelcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, SafeAreaView, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { useNavigation } from '@react-navigation/native';
+
+const assistantwelcome = () =>{
+
+}
+
+export default WelcomeScreen;
